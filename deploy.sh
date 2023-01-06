870060a759682023c579222a190a77e559a59c91
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-06 12:47:00 -0300

    feat: versioning fission deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
 fission spec init
-fission env create --spec --name get-user-data-env --image nexus.sigame.com.br/fission-async:0.1.7 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
-fission fn create --spec --name get-user-data-fn --env get-user-data-env --src "./func/*" --entrypoint main.get_user_data --executortype newdeploy --maxscale 1
-fission route create --spec --name get-user-data-rt --method GET --url /account/get_user_data --function get-user-data-fn
+fission env create --spec --name acc-data-get-env --image nexus.sigame.com.br/fission-account-data-get:0.1.0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name acc-data-get-fn --env acc-data-get-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
+fission route create --spec --name acc-data-get-rt --method GET --url /account/get_user_data --function acc-data-get-fn
